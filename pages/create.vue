93094786c1297e6424b5bafc12a6103c542abf0b
BISMINSHA1 <62139530+BISMINSHA1@users.noreply.github.com>
2020-03-19 13:58:03 +0530

    second

--- pages/create.vue
+++ pages/create.vue
@@ -2,7 +2,7 @@
   <div>
     <v-row>
       <v-col cols="12" sm="6">
-        <v-text-field  label="Employee Id" single-line></v-text-field>
+        <v-text-field label="Employee Id" single-line></v-text-field>
       </v-col>
     </v-row>
     <v-row>
@@ -20,10 +20,8 @@
         <v-text-field label="Employee Age" single-line></v-text-field>
       </v-col>
       <v-col class="text-center"  sm="11">
-      <v-btn small color="primary" @click="save">Submit</v-btn>
+        <v-btn small color="primary" @click="save">Submit</v-btn>
       </v-col>
     </v-row>
   </div>
 </template>
-
-<script>
--- pages/read.vue
+++ pages/read.vue
@@ -1,75 +1,32 @@
 <template>
-<div>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+    outlined
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="overline mb-10">ID</div>
+        <v-list-item-title class="headline mb-1">EMPLOYEE NAME</v-list-item-title>
+        <v-list-item-subtitle>AGE</v-list-item-subtitle>
+        <v-list-item-subtitle>SALARY</v-list-item-subtitle>
+      </v-list-item-content>
 
-<head>
-  <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
-</head>
+      <v-list-item-avatar
+        tile
+        size="80"
+        color="grey"
+      ></v-list-item-avatar>
+    </v-list-item>
+  </v-card>
+</template>
+<script>
+export default {
+  mounted: function(){
+    axios.get('http://dummy.restapiexample.com/api/v1/employees')
+    .then(response => console.log(response))
+  }
+  
+}
+</script>
 
-  <div id="app"
-       class="ui container">
-    <h1>Employee Details</h1>
-    <div class="ui cards">
-      <employee-details 
-        v-for="employeename in employeenames"
-        :employeename="employeename"
-      ></employee-details>
-    </div>
-    <script type="text/x-template" id="employee-id-template">
-    <div class="ui card">
-      <div class="image">
-        <img :src="user.avatar_url">
-      </div>
-      <div class="content">
-        <a :href="`https://github.com/${employeename}`" class="header">{{user.name}}</a>
-        <div class="meta">
-          <span class="date">Joined in {{user.created_at}}</span>
-        </div>
-        <div class="description">
-          {{user.bio}}
-        </div>
-      </div>
-      <div class="extra content">
-        <a :href="`https://github.com/${employeename}?tab=followers`">
-          <i class="user icon"></i>
-          {{user.followers}} Friends
-        </a>
-      </div>
-    </div>
-  </script>
-</div>
-</template> 
-  <script type="text/x-template" id="employee-id-template">
-  </script>
-
-  <script>
-      vue.component('employee-details',{
-          template: '#employee-id-template',
-          props:{
-              employeename:{
-                  type: string,
-                  required: true
-
-              }
-
-          },
-          data(){
-              return{
-                  user:{}
-              }
-
-          },
-          created(){
-              axios.get(`http://dummy.restapiexample.com/api/v1/employees/${this.employeename}`)
-              .then(respons => {
-                  this.user = response.data
-              })
-          }
-      })
-    new Vue({
-      el: '#app',
-      data: {
-        usernames: []
-      }
-    })
-  </script>
